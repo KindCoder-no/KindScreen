2a4617c4432529f83c305834bb8ff8a055764d92
Emre Sanden <emresanden@gmail.com>
2023-01-30 23:06:26 +0100

    Update version checker

--- components/Updates.tsx
+++ components/Updates.tsx
@@ -2,18 +2,18 @@ import { Alert, AlertTitle } from "@mui/material";
 import React from "react";
 
 export default function Updates() {
-  const [installedVersion, setInstalledVersion] = React.useState<string>();
-  const [currentVersion, setCurrentVersion] = React.useState<string>();
+  const [installedVersion, setInstalledVersion] = React.useState<any>();
+  const [currentVersion, setCurrentVersion] = React.useState<any>();
   React.useEffect(() => {
-    fetch("/version.txt")
-      .then((res) => res.text())
+    fetch("/version.json")
+      .then((res) => res.json())
       .then((data) => setInstalledVersion(data));
 
     const fetchCurrentVersion = () => {
       fetch(
-        "https://raw.githubusercontent.com/KindCoder-no/KindScreen/main/public/version.txt"
+        "https://raw.githubusercontent.com/KindCoder-no/KindScreen/main/public/version.json"
       )
-        .then((res) => res.text())
+        .then((res) => res.json())
         .then((data) => setCurrentVersion(data));
     };
 
@@ -27,14 +27,15 @@ export default function Updates() {
   }, []);
   return (
     <>
-      {installedVersion != currentVersion && (
+      {installedVersion?.current != currentVersion?.current && (
         <Alert severity="info">
           <AlertTitle>Update</AlertTitle>
           An update for this software is available. You are currently running
-          version: <strong>{installedVersion}</strong>, and new version is:{" "}
-          <strong>{currentVersion}</strong>.<br></br>
+          version: <strong>{installedVersion?.current}</strong>, and new version
+          is: <strong>{currentVersion?.current}</strong>.<br></br>
           Update is on{" "}
-          <a href="https://github.com/KindCoder-no/KindScreen">Github</a>
+          <a href="https://github.com/KindCoder-no/KindScreen">Github</a>.{" "}
+          Changes: {currentVersion?.message}
         </Alert>
       )}
     </>
